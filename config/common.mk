1f294c40fbbade3d4de395a4aff5fedbd362d8ca
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-02 13:48:03 -0400

    Bingo
    Bingo

--- config/common.mk
+++ config/common.mk
@@ -363,4 +363,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 -include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
-$(call inherit-product, vendor/light/opengapps.mk)
+$(call inherit-product, vendor/light/config/opengapps.mk)
