e2921b44456d629e5cfeb6e101af41a3bb0a4832
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-03 00:10:05 -0400

    Revert "Temp Hack Because I Miss Up"
    This reverts commit a50493c03a1ad69a3f712509a4160bedd1dca748.

--- config/opengapps_full_phone.mk
+++ config/opengapps_full_phone.mk
@@ -1,33 +1,29 @@
 # Open Gapps PreLoaded
-GAPPS_VARIANT := nano
+
 GAPPS_VARIANT := super
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-#Other Apps I Need 
-GAPPS_PRODUCT_PACKAGES += \
-    CalendarGoogle \
-    ExchangeGoogle \
-    Gmail \
-    CalculatorGoogle \
-    ClockGoogle \
-    Maps \
-    Photos \
-    YouTube \
-    Drive \
-    Music \
-    CameraGoogle \
-    ContactsGoogle \
-    KeyboardGoogle \
-    PixelIcons \
-    StorageManagerGoogle \
-    GCS \
-    TalkBack \
-    PrintServiceGoogle \
-    WebViewGoogle \
-    WebViewStub \
-    #Wallpapers \
-    #Newsstand \
-    #NewsWidget \
-    #PlayGames 
+
+# Open Gapps Package I Don't Want At All
+GAPPS_EXCLUDED_PACKAGES := \
+    Wallpapers \
+    CloudPrint \
+    DMAgent \
+    Docs \
+    Duo \
+    Earth\
+    GoogleNow \
+    GooglePay \
+    GooglePlus \
+    Hangouts \
+    Indic \
+    Japanese \
+    Keep \
+    Korean \
+    NewsStand \
+    NewsWidget \
+    Pinyin \
+    ProjectFi \
+    Sheets \
 
 # Setting
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
--- config/opengapps_full_tablet_wifionly.mk
+++ config/opengapps_full_tablet_wifionly.mk
@@ -1,37 +1,34 @@
 # Open Gapps PreLoaded
-GAPPS_VARIANT := nano
+
 GAPPS_VARIANT := super
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-#Other Apps I Need 
-GAPPS_PRODUCT_PACKAGES += \
-    CalendarGoogle \
-    ExchangeGoogle \
-    Gmail \
-    CalculatorGoogle \
-    ClockGoogle \
-    Maps \
-    Photos \
-    YouTube \
-    Drive \
-    Music \
-    CameraGoogle \
-    ContactsGoogle \
-    KeyboardGoogle \
-    PixelIcons \
-    StorageManagerGoogle \
-    GCS \
-    TalkBack \
-    PrintServiceGoogle \
+
+# Open Gapps Package I Don't Want At All
+GAPPS_EXCLUDED_PACKAGES := \
+    Wallpapers \
     CarrierServices \
+    CloudPrint \
+    DialerFramework \
     DialerGoogle \
-    Messenger \
-    TagGoogle \
-    GooglePay \
+    DMAgent \
+    Docs \
+    Duo \
+    Earth\
     Fitness \
-    #Wallpapers \
-    #Newsstand \
-    #NewsWidget \
-    #PlayGames 
+    GoogleNow \
+    GooglePay \
+    GooglePlus \
+    Hangouts \
+    Indic \
+    Japanese \
+    Keep \
+    Korean \
+    Messenger \
+    NewsStand \
+    NewsWidget \
+    Pinyin \
+    ProjectFi \
+    Sheets \
 
 # Setting
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
