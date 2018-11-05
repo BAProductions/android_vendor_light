bc7522320579501d2867572c7a09ba7922634ba1
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-04 21:44:35 -0500

    Boot
    Boot

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -15,7 +15,7 @@
 #
 
 define build-bootanimation
-    sh vendor/mk/bootanimation/generate-bootanimation.sh \
+    sh vendor/light/bootanimation/generate-bootanimation.sh \
     $(TARGET_SCREEN_WIDTH) \
     $(TARGET_SCREEN_HEIGHT) \
     $(TARGET_BOOTANIMATION_HALF_RES)
