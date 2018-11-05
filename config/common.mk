3f2d29799d0862300c0823d5e76862e03676bf53
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-05 14:05:32 -0500

    Added MakBootScreen.mk & ResetBootScreen.mk)
    Added MakBootScreen.mk & ResetBootScreen.mk)

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,7 @@ PRODUCT_BRAND ?= LightOS
 Base_Vendor_Name := cm
 Base_Vendor_Name_full := cyanogenmod
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
+$(call inherit-product, vendor/light/config/MakeBootScreen.mk)
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google
@@ -94,7 +94,7 @@ endif
 
 # Bootanimation
 PRODUCT_PACKAGES += \
-    bootanimation.tar
+    bootanimation.zip
 
 # Required CM packages
 PRODUCT_PACKAGES += \
@@ -373,3 +373,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 -include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product, vendor/light/config/ResetBootScreen.mk)
