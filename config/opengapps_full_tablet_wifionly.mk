c348ac4a9de1ad8087398266d87a34548dd9b667
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-11-05 20:11:58 -0500

    Bingo
    Bingo

--- config/opengapps_full_tablet_wifionly.mk
+++ config/opengapps_full_tablet_wifionly.mk
@@ -1,10 +1,52 @@
 # Open Gapps PreLoaded
 
-GAPPS_VARIANT := stock
+GAPPS_VARIANT := super
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 # Open Gapps Package I Don't Want At All
-#GAPPS_EXCLUDED_PACKAGES := \
+#Apps I Really Want In My Custom Rom
+GAPPS_PRODUCT_PACKAGES +=\
+        FaceLock \
+        libfilterpack_facedetect \
+        Drive \
+        GoogleCalendarSyncAdapter \
+        GoogleContactsSyncAdapter \
+        Maps \
+        YouTube \
+        talkback \
+        CalculatorGoogle \
+        GoogleBackupTransport \
+        GoogleLoginService \
+        GooglePartnerSetup \
+        GoogleCamera \
+        GoogleContacts \
+        GooglePrintRecommendationService \
+        GoogleServicesFramework \
+        GoogleTTS \
+        LatinImeGoogle \
+        Music2 \
+        PixelLauncherIcons \
+        WebViewGoogle \
+        Turbo \
+        AndroidPlatformServices \
+        Chrome \
+        GoogleExtServices \
+        GoogleFeedback \
+        GoogleOneTimeInitializer \
+        GooglePackageInstaller \
+        Photos \
+        SetupWizard \
+        GCS \
+        Phonesky \
+        CalendarGooglePrebuilt \
+        PrebuiltDeskClockGoogle \
+        PrebuiltExchange3Google \
+        PrebuiltGmail \
+        PrebuiltGmsCore \
+        PrebuiltGmsCoreInstantApps \
+        GmsCoreSetupPrebuilt \
+
+GAPPS_EXCLUDED_PACKAGES := \
 		Books \
 		CloudPrint2 \
 		DMAgent \
